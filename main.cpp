7d45321dfbf1b058b381806e81996543688526db
Raúl Ulises Martín Hernández <martin.jurses@gmail.com>
2017-05-14 11:18:43 +0100

    Al parecer todo funciona 10/10
    Falta el método por traza, el estadístico,
    la implementación de matrícula y buscar errores.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include "arbolBin.hpp"
 #include "nodo.hpp"
+#include <fstream>
+
+template<class T>
+void lector(const char* nombre, arbolBin_t<T>* arbol){
+	char c;
+	int x;
+	std::ifstream archivo;
+	if(nombre)
+		archivo.open(nombre, std::ifstream::in);
+	else
+		archivo.open("lata.txt", std::ifstream::in);
+
+	if(archivo.is_open()){
+		while (archivo.good()) {
+			archivo >> c;
+			archivo >> (int&)x;
+			switch(c){
+				case 'i':
+				case 'I':	arbol->insertar(x);
+							std::cout << "A insertar: " << x << std::endl;
+							break;
+				case 'e':
+				case 'E':	arbol->eliminar(x);
+							std::cout << "A eliminar: " << x << std::endl;
+							break;
+			}
+		}
+		archivo.close();
+	}
+}
 
 int main(void){
 	arbolBin_t<int> arbol;
-	arbol.insertar(8);
-	arbol.eliminar(8);
-	arbol.insertar(8);
-	arbol.insertar(14);
-	arbol.insertar(38);
-	//arbol.insertar(7);
-	//arbol.insertar(2);
-	arbol.insertar(23);
+	lector("x.txt", &arbol);
 }
